528441f04c83ffaf795cf813eb80ddb647a0eb8f
GeraldRyan <gerald.ryan40@yahoo.com>
2020-10-30 22:15:05 -0500

    passed pointers exercise

--- pointers/pointers.c
+++ pointers/pointers.c
@@ -63,40 +63,52 @@ char *find_char(char *str, char c)
 
     Do not use the `strstr` function from the standard library.
 */
-// char *find_string(char *haystack, char *needle)
-// {
-//     char* pHaystack = haystack;
-//     printf("Haystack pointer %s\n", pHaystack);
+char *find_string(char *haystack, char *needle)
+{
+    char *pHaystack = haystack;
+    printf("Haystack pointer %s\n", pHaystack);
 
-//     int i, j, lhay = 0, lneedle = 0;
-    
-//     while (haystack[lhay] != 0)
-//     {
-//         lhay++;
-//     }
-//         // printf("length hay %i", lhay);
-//     while (needle[lneedle] != 0)
-//     {
-//         lneedle++;
-//     }
-//         // printf("length needle %i", lneedle);
+    int i, j, lhay = 0, lneedle = 0;
 
+    while (haystack[lhay] != 0)
+    {
+        lhay++;
+    }
+    // printf("length hay %i", lhay);
+    while (needle[lneedle] != 0)
+    {
+        lneedle++;
+    }
+    // printf("length needle %i", lneedle);
 
-//     for (i=0; i < lhay; i++)
-//     {
-//         for (j=0; j < lneedle; j++)
-//         {
-//             printf("i %i j %i Needle %c and Haystack %c\n", i, j, needle[j], haystack[i+j]);
-//             if (needle[j] != haystack[i+j]){
-//                 break;
-//             }
-//             pHaystack = haystack+i;
-//             return pHaystack;
-//         }
-//     }
-//     char* n = "false";
-//     return n;
-// }
+    for (i = 0; i < lhay; i++)
+    {
+        for (j = 0; j < lneedle; j++)
+        {
+            printf("i %i j %i Needle %c and Haystack %c\n", i, j, needle[j], haystack[i + j]);
+            if (needle[j] == haystack[i + j])
+            {
+                printf("continuing\n");
+                if (j == lneedle-1)
+                {
+                    pHaystack = haystack + i;
+                    return pHaystack;
+                }
+                else
+                {
+                    continue;
+                }
+            }
+            else
+            {
+                printf("breaking\n");
+                break;
+            }
+        }
+    }
+    char *n = NULL;
+    return n;
+}
 
 #ifndef TESTING
 int main(void)
@@ -107,10 +119,10 @@ int main(void)
 
     // string_copy(empty, string);
 
-    char *found_char = find_char("hello", 'l');
-    printf("Found Char: %c address %p\n", found_char[0], found_char);
-    // char *found_string = find_string("world", "or");
-    // printf("Found String: %s \nlocation: %p\n", found_string, found_string);
+    // char *found_char = find_char("hello", 'l');
+    // printf("Found Char: %c address %p\n", found_char[0], found_char);
+    char *found_string = find_string("world", "or");
+    printf("Found String: %s \n", found_string);
 
     // printf("Found char: %s\n", found_char);
     // printf("Found string: %s\n", found_string);
