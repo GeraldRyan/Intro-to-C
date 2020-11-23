448f4d7b8537dac4f858634fc32c2666b911b925
GeraldRyan <gerald.ryan40@yahoo.com>
2020-11-23 17:55:55 -0600

    laying out framework for hangman

--- hangman/hangman.c
+++ hangman/hangman.c
@@ -2,20 +2,33 @@
 #include <stdlib.h>
 #include <string.h>
 
-char HANG_STATES[7][10 * 9] =
+char HANG_STATES[7][10 * 9 + 1] =
 		{
-				"             +         +----     +----     +----     +----     +----     +----     +----  ",
-				"             |         |         |   O     |   O     |   O     |   O     |   O     |   O  ",
-				"             |         |         |         |   +     | --+     | --+--   | --+--   | --+--",
-				"             |         |         |         |   |     |   |     |   |     |   |     |   |  ",
-				"             |         |         |         |         |         |         |  /      |  / \\ ",
-				"             |         |         |         |         |         |         |         |      ",
-				"/*****\\   /*****\\   /*****\\   /*****\\   /*****\\   /*****\\   /*****\\   /*****\\   /*****\\   "};
+				"             +         +----     +----     +----     +----     +----     +----     +----  \0",
+				"             |         |         |   O     |   O     |   O     |   O     |   O     |   O  \0",
+				"             |         |         |         |   +     | --+     | --+--   | --+--   | --+--\0",
+				"             |         |         |         |   |     |   |     |   |     |   |     |   |  \0",
+				"             |         |         |         |         |         |         |  /      |  / \\ \0",
+				"             |         |         |         |         |         |         |         |      \0",
+				"/*****\\   /*****\\   /*****\\   /*****\\   /*****\\   /*****\\   /*****\\   /*****\\   /*****\\   \0"};
 
-void print_each(int * a){
+void print_each_i(int *a)
+{
 	printf("Elements of the array:\n");
-	for (int i = 0; i< sizeof(a)/sizeof(int); i++){
+	for (int i = 0; i < sizeof(a) / sizeof(int); i++)
+	{
 		printf("%i ", a[i]);
+	}
+	printf("\n");
+}
+void print_each_s(char *s)
+{
+	// char* s = a;
+	printf("String %s\n", s);
+	printf("Elements of the string:\n");
+	for (int i = 0; i < strlen(s); i++)
+	{
+		printf("%c ", s[i]);
 	}
 	printf("\n");
 }
@@ -26,19 +39,13 @@ int *find_chars(char *s, char c)
 	int inst = 0;
 	for (int i = 0; i < strlen(s) - 1; i++)
 	{
-		// printf("answer: %s, i is %i, s[i] %c, char guess: %c\n", s, i, s[i], c);
 		if (s[i] == c)
 		{
-			// printf("Matching\n");
-			// printf("inst: %i  i %i \n", inst, i);
+
 			locs[inst] = i;
-			// printf("locs[inst] %i", locs[inst]);
 			inst++;
 		}
 	}
-	// printf("PRINTING EACH\n");
-	// print_each(locs);
-	// printf("LOCS: %i", locs);
 	return locs;
 }
 
@@ -51,17 +58,18 @@ int main(int argc, char *argv[])
 	char guess;
 	char guesses[8];
 	int *result;
+	print_each_s(answer);
 	for (int i = 0; i < num_tries; i++)
 	{
 		printf("Guess a letter: ");
 		scanf(" %c", &guess);
 		printf("\nGuess: %c \n", guess);
 		result = find_chars(answer, guess);
-		printf("Results length is %i\n", sizeof(result)/sizeof(int));
-		print_each(result);
+		printf("Results length is %i\n", sizeof(result) / sizeof(int));
+		print_each_i(result);
+		printf(" ");
 		i++;
 	}
-	// printf("%s", HANG_STATES[2]);
 
 	return 0;
 }
